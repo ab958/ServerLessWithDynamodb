1be17c520efc4d12a1236d451efc475e2c29c464
Abdul Wahab <abwahab958@gmail.com>
2021-10-13 07:38:21 -0700

    final

---
+++ serverless.ts
@@ -0,0 +1,81 @@
+import type { AWS } from "@serverless/typescript";
+
+import hello from "@functions/hello";
+
+const serverlessConfiguration: AWS = {
+  service: "serverless",
+  frameworkVersion: "2",
+  custom: {
+    dynamodb: {
+      stages: ["dev"],
+      start: {
+        port: 8000,
+        inMemory: true,
+        heapInitial: "200m",
+        heapMax: "1g",
+        migrate: true,
+        seed: true,
+        convertEmptyValues: true,
+      },
+    },
+
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
+    },
+  },
+  plugins: [
+    "serverless-esbuild",
+    "serverless-dynamodb-local",
+    "serverless-offline",
+  ],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+    },
+    lambdaHashingVersion: "20201221",
+  },
+  // import the function via paths
+  functions: { hello },
+
+  resources: {
+    Resources: {
+      usersTable: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "usersTableAAA",
+          AttributeDefinitions: [
+            {
+              AttributeName: "email",
+              AttributeType: "S",
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: "email",
+              KeyType: "HASH",
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+        },
+      },
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
